2abd67b321af262b96b1f72dedfb660a132b3592
gitclonparis <gitclon@google.com>
2024-09-25 17:39:20 +0200

    Description des modifications

--- VolumetriFilter.cs
+++ VolumetriFilter.cs
@@ -33,29 +33,29 @@ namespace NinjaTrader.NinjaScript.Indicators.ninpai
         #region Properties
         // Paramètres pour la flèche UP
         [NinjaScriptProperty]
-        [Display(Name = "Activer la condition UP", Order = 1, GroupName = "Paramètres UP")]
-        public bool UpConditionEnabled { get; set; }
+        [Display(Name = "Activer la condition UP", Order = 1, GroupName = "0.01_Paramètres UP")]
+        public bool UpBarDeltaEnabled { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Seuil inférieur UP (BarDelta)", Order = 2, GroupName = "Paramètres UP")]
-        public double UpLowerThreshold { get; set; }
+        [Display(Name = "Seuil  supérieur UP (BarDelta)", Order = 2, GroupName = "0.01_Paramètres UP")]
+        public double MinBarDeltaUPThreshold { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Seuil supérieur UP (BarDelta)", Order = 3, GroupName = "Paramètres UP")]
-        public double UpUpperThreshold { get; set; }
+        [Display(Name = "Seuil inférieur UP (BarDelta)", Order = 3, GroupName = "0.01_Paramètres UP")]
+        public double MaxBarDeltaUPThreshold { get; set; }
 
         // Paramètres pour la flèche DOWN
         [NinjaScriptProperty]
-        [Display(Name = "Activer la condition DOWN", Order = 1, GroupName = "Paramètres DOWN")]
-        public bool DownConditionEnabled { get; set; }
+        [Display(Name = "Activer la condition DOWN", Order = 1, GroupName = "0.02_Paramètres DOWN")]
+        public bool DownBarDeltaEnabled { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Seuil inférieur DOWN (BarDelta)", Order = 2, GroupName = "Paramètres DOWN")]
-        public double DownLowerThreshold { get; set; }
+        [Display(Name = "Seuil  supérieur DOWN (BarDelta)", Order = 2, GroupName = "0.02_Paramètres DOWN")]
+        public double MinBarDeltaDownThreshold { get; set; }
 
         [NinjaScriptProperty]
-        [Display(Name = "Seuil supérieur DOWN (BarDelta)", Order = 3, GroupName = "Paramètres DOWN")]
-        public double DownUpperThreshold { get; set; }
+        [Display(Name = "Seuil inférieur DOWN (BarDelta)", Order = 3, GroupName = "0.02_Paramètres DOWN")]
+        public double MaxBarDeltaDownThreshold { get; set; }
 
         // Couleurs des flèches
         [XmlIgnore]
@@ -102,14 +102,14 @@ namespace NinjaTrader.NinjaScript.Indicators.ninpai
                 IsSuspendedWhileInactive                       = true;
 
                 // Paramètres par défaut pour UP
-                UpConditionEnabled                             = true;
-                UpLowerThreshold                               = 500;
-                UpUpperThreshold                               = 2000;
+                UpBarDeltaEnabled                              = true;
+                MinBarDeltaUPThreshold                         = 500;
+                MaxBarDeltaUPThreshold                         = 2000;
 
                 // Paramètres par défaut pour DOWN
-                DownConditionEnabled                           = true;
-                DownLowerThreshold                             = -2000;
-                DownUpperThreshold                             = -500;
+                DownBarDeltaEnabled                            = true;
+                MinBarDeltaDownThreshold                       = -500;
+                MaxBarDeltaDownThreshold                       = -2000;
 
                 // Couleurs par défaut
                 UpArrowColor                                   = Brushes.Green;
@@ -133,18 +133,18 @@ namespace NinjaTrader.NinjaScript.Indicators.ninpai
             var barDelta = barsType.Volumes[CurrentBar].BarDelta;
 
             // Condition pour flèche UP
-            if (UpConditionEnabled)
+            if (UpBarDeltaEnabled)
             {
-                if (barDelta > UpLowerThreshold && barDelta < UpUpperThreshold)
+                if (barDelta > MinBarDeltaUPThreshold && barDelta < MaxBarDeltaUPThreshold)
                 {
                     Draw.ArrowUp(this, "UpArrow" + CurrentBar, false, 0, Low[0] - TickSize, UpArrowColor);
                 }
             }
 
             // Condition pour flèche DOWN
-            if (DownConditionEnabled)
+            if (DownBarDeltaEnabled)
             {
-                if (barDelta > DownLowerThreshold && barDelta < DownUpperThreshold)
+                if (barDelta < MinBarDeltaDownThreshold && barDelta > MaxBarDeltaDownThreshold)
                 {
                     Draw.ArrowDown(this, "DownArrow" + CurrentBar, false, 0, High[0] + TickSize, DownArrowColor);
                 }
@@ -152,3 +152,60 @@ namespace NinjaTrader.NinjaScript.Indicators.ninpai
         }
     }
 }
+
+#region NinjaScript generated code. Neither change nor remove.
+
+namespace NinjaTrader.NinjaScript.Indicators
+{
+	public partial class Indicator : NinjaTrader.Gui.NinjaScript.IndicatorRenderBase
+	{
+		private ninpai.VolumetriFilter[] cacheVolumetriFilter;
+		public ninpai.VolumetriFilter VolumetriFilter(bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			return VolumetriFilter(Input, upBarDeltaEnabled, minBarDeltaUPThreshold, maxBarDeltaUPThreshold, downBarDeltaEnabled, minBarDeltaDownThreshold, maxBarDeltaDownThreshold);
+		}
+
+		public ninpai.VolumetriFilter VolumetriFilter(ISeries<double> input, bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			if (cacheVolumetriFilter != null)
+				for (int idx = 0; idx < cacheVolumetriFilter.Length; idx++)
+					if (cacheVolumetriFilter[idx] != null && cacheVolumetriFilter[idx].UpBarDeltaEnabled == upBarDeltaEnabled && cacheVolumetriFilter[idx].MinBarDeltaUPThreshold == minBarDeltaUPThreshold && cacheVolumetriFilter[idx].MaxBarDeltaUPThreshold == maxBarDeltaUPThreshold && cacheVolumetriFilter[idx].DownBarDeltaEnabled == downBarDeltaEnabled && cacheVolumetriFilter[idx].MinBarDeltaDownThreshold == minBarDeltaDownThreshold && cacheVolumetriFilter[idx].MaxBarDeltaDownThreshold == maxBarDeltaDownThreshold && cacheVolumetriFilter[idx].EqualsInput(input))
+						return cacheVolumetriFilter[idx];
+			return CacheIndicator<ninpai.VolumetriFilter>(new ninpai.VolumetriFilter(){ UpBarDeltaEnabled = upBarDeltaEnabled, MinBarDeltaUPThreshold = minBarDeltaUPThreshold, MaxBarDeltaUPThreshold = maxBarDeltaUPThreshold, DownBarDeltaEnabled = downBarDeltaEnabled, MinBarDeltaDownThreshold = minBarDeltaDownThreshold, MaxBarDeltaDownThreshold = maxBarDeltaDownThreshold }, input, ref cacheVolumetriFilter);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.MarketAnalyzerColumns
+{
+	public partial class MarketAnalyzerColumn : MarketAnalyzerColumnBase
+	{
+		public Indicators.ninpai.VolumetriFilter VolumetriFilter(bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			return indicator.VolumetriFilter(Input, upBarDeltaEnabled, minBarDeltaUPThreshold, maxBarDeltaUPThreshold, downBarDeltaEnabled, minBarDeltaDownThreshold, maxBarDeltaDownThreshold);
+		}
+
+		public Indicators.ninpai.VolumetriFilter VolumetriFilter(ISeries<double> input , bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			return indicator.VolumetriFilter(input, upBarDeltaEnabled, minBarDeltaUPThreshold, maxBarDeltaUPThreshold, downBarDeltaEnabled, minBarDeltaDownThreshold, maxBarDeltaDownThreshold);
+		}
+	}
+}
+
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public partial class Strategy : NinjaTrader.Gui.NinjaScript.StrategyRenderBase
+	{
+		public Indicators.ninpai.VolumetriFilter VolumetriFilter(bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			return indicator.VolumetriFilter(Input, upBarDeltaEnabled, minBarDeltaUPThreshold, maxBarDeltaUPThreshold, downBarDeltaEnabled, minBarDeltaDownThreshold, maxBarDeltaDownThreshold);
+		}
+
+		public Indicators.ninpai.VolumetriFilter VolumetriFilter(ISeries<double> input , bool upBarDeltaEnabled, double minBarDeltaUPThreshold, double maxBarDeltaUPThreshold, bool downBarDeltaEnabled, double minBarDeltaDownThreshold, double maxBarDeltaDownThreshold)
+		{
+			return indicator.VolumetriFilter(input, upBarDeltaEnabled, minBarDeltaUPThreshold, maxBarDeltaUPThreshold, downBarDeltaEnabled, minBarDeltaDownThreshold, maxBarDeltaDownThreshold);
+		}
+	}
+}
+
+#endregion
